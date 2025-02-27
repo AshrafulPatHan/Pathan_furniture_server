7141302e726c39c8680897e2fb565c1066c36cd7
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-02-27 17:31:01 +0600

    add .env file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = 3000
 
@@ -11,14 +12,15 @@ app.listen(port, () => {
 })
 
 
-// Pathan_furniture
-// 3IXtairxVYGAIXEU
+// 
+// 
 
 // Mongo db
 
+console.log(`THIS IS a ------------------------${process.env.DB_USER_NAME}`);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.zxihh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.zxihh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,6 +36,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    // start ----------
+
+    
+
+
+    // End ----------
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
